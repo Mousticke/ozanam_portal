db87592c8f62b47f77240c1558b68dd381ce17ad
Akim <abdelhakim.ben_chiha@ig2i.fr>
2016-06-03 11:28:21 +0200

    Ajustement du PHP + upload des images sur git

---
+++ database/migrations/2016_06_01_093031_create_links_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('post_id');
+            $table->string('body');
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('links');
+    }
+}
